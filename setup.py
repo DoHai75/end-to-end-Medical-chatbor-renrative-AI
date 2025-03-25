ac62cb7ba304f6f8ffc91a2ed314158ffb6e18a7
Minh Hai <dominhhai752@gmail.com>
2025-03-25 22:14:25 +0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Minh Hai',
+    author_email='dominhhai752@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
